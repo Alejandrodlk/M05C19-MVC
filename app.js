64dcb8362e710cde33273d85b96172b56edfa8a3
Alejandrodlk <ale_165851@hotmail.com>
2022-05-02 11:37:17 -0300

    Estructura inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+const mainRouter = require("./routes/main")
+
+
+/* Recursos estaticos */
+app.use(express.static(path.resolve(__dirname , "public")))
+
+
+/* seteo motor de plantilla */
+app.set("views" , path.resolve(__dirname , "views"))
+app.set("view engine" , "ejs")
+
+
+
+/* app.get("/" , (req,res) => res.sendFile(path.resolve(__dirname , "views","home.html"))) */
+app.use("/" , mainRouter)
+
+
+
+app.listen(port , () => console.log(`servidor corriendo en el puerto http://localhost:${port}`))
---
+++ controllers/mainController.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    home : (req,res) => {
+        return res.render("home")
+    },
+
+    about : (req,res) => {
+        return res.render("about")
+    }
+
+}
